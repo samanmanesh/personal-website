5a075b3d5480d54fa0b934949fd08c26d011e8c3
Saman <samanmanesh7@gmail.com>
2024-10-09 14:05:54 -0400

    Setup the font and about to start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        soehne: ['Soehne', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
