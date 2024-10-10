2f1b1b264bf97ceb15847a76d257d43d6ffaec9b
Saman <samanmanesh7@gmail.com>
2024-10-10 17:55:51 -0400

    added dark mode

--- app/(index)/components/SelectedProject.tsx
+++ app/(index)/components/SelectedProject.tsx
@@ -13,7 +13,7 @@ const SelectedProject = ({className}: SelectedProjectProps) => {
       Selected Project
     </h2>
     {/* video */}
-    <div className='relative w-full  rounded-md border border-[#E8E8E8] aspect-video'>
+    <div className='relative w-full  rounded-md border border-[#E8E8E8] dark:border-none aspect-video'>
       <video className='w-full h-full object-cover' autoPlay loop muted>
         <source src='/videos/selected-project.mp4' type='video/mp4' />
       </video>
--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -5,8 +5,8 @@ import SelectedProject from "./components/SelectedProject";
 export default function Home() {
   return (
     <main className="pt-16 md:pt-32 grid items-center justify-items-center min-h-screen overflow-auto ">
-      <nav className="h-28 w-full bg-gradient-to-t from-transparent to-stone-50 fixed top-0 z-10"></nav>
-      <section className="h-full md:w-2/5 text-primary">
+      <nav className="h-28 w-full bg-gradient-to-t from-transparent to-stone-100 dark:to-[#1e1e1e]  mix-blend-normal backdrop-grayscale fixed top-0 z-10"></nav>
+      <section className="h-full md:w-2/5 text-primary dark:text-white">
         <Intro className="mb-16" />
         <SelectedProject className="mb-16" />
         <Projects className="mb-16" />
