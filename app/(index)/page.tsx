3e9ff6c5905bd314043d9d60a99300618ae74b5a
Saman <samanmanesh7@gmail.com>
2024-10-09 20:17:13 -0400

    Added fading nav bar to the top of the page

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,4 +1,3 @@
-
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
 import SelectedProject from "./components/SelectedProject";
@@ -6,11 +5,11 @@ import SelectedProject from "./components/SelectedProject";
 export default function Home() {
   return (
     <main className="pt-16 md:pt-32 grid items-center justify-items-center min-h-screen overflow-auto ">
+      <nav className="h-28 w-full bg-gradient-to-t from-transparent to-stone-50 fixed top-0 z-10"></nav>
       <section className="h-full md:w-2/5 text-primary">
         <Intro className="mb-16" />
-        <SelectedProject className="mb-16"/>
+        <SelectedProject className="mb-16" />
         <Projects className="mb-16" />
-
       </section>
     </main>
   );
