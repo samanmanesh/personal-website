18c73f07c5d5fd1f9426bf83e1538055f2741c5e
Saman <samanmanesh7@gmail.com>
2024-10-12 16:00:26 -0400

    Contact is done

--- app/(index)/components/Contact.tsx
+++ app/(index)/components/Contact.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/app/utils/cn";
+import Link from "next/link";
 import React from "react";
 
 type ContactProps = {
@@ -9,15 +10,15 @@ const Contact = ({ className }: ContactProps) => {
   return (
     <div className={cn("flex flex-col gap-7", className)}>
       <h2 className="font-inter font-medium text-base">Contact</h2>
-
       <p>
-        Reach me at @
-        <a
+        Reach me at &nbsp;
+        <Link
           href="https://x.com/saman_manesh"
           className="underline decoration-secondary underline-offset-4"
+          target="_blank"
         >
-          saman_manesh
-        </a>{" "}
+          @saman_manesh
+        </Link>{" "}
         or{" "}
         <a
           href="mailto:samansbm7@gmail.com"
