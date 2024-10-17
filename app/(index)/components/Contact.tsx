6c020b7ee3ae415c38df6a9d295d80f5a94b992d
Saman <samanmanesh7@gmail.com>
2024-10-17 12:01:43 -0400

    check

--- app/(index)/components/Contact.tsx
+++ app/(index)/components/Contact.tsx
@@ -10,21 +10,29 @@ const Contact = ({ className }: ContactProps) => {
   return (
     <div className={cn("flex flex-col gap-10 ", className)}>
       <h2 className="font-inter font-medium text-base">Contact</h2>
-      <div className="flex gap-1 ">
-        Reach me at
-        <ExternalLink
-          link="https://x.com/saman_manesh"
-          className="text-base text-primary font-normal "
-          target="_blank"
-        >
-          @saman_manesh
-        </ExternalLink>
-        <span> or </span>
-        <ExternalLink link="mailto:samansbm7@gmail.com "
-        className="text-base text-primary font-normal "
-        >
-          samansbm7@gmail.com
-        </ExternalLink>
+      <div className="flex flex-col md:flex-row  gap-1 ">
+        <p className="flex-shrink-0">Reach me at</p>
+        <div className="flex gap-1">
+          <ExternalLink
+            link="https://x.com/saman_manesh"
+            className="text-base text-primary font-normal "
+            target="_blank"
+          >
+            <span className="text-base text-primary font-normal ">
+            @saman_manesh
+            </span>
+          </ExternalLink>
+          <span> or </span>
+          <ExternalLink
+            link="mailto:samansbm7@gmail.com "
+            
+          >
+            <span className="text-base text-primary font-normal ">
+
+            samansbm7@gmail.com
+            </span>
+          </ExternalLink>
+        </div>
       </div>
     </div>
   );
